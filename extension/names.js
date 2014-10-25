d112c81dcf34ce918793f39cc9659fb2e86ac6da
Deepak Padamata <deepakpadamata@gmail.com>
2014-10-25 22:15:56 +0530

    folder structure

--- extension/names.js
+++ extension/names.js
@@ -1,9 +1,12 @@
 chrome.tabs.getAllInWindow(null, function(tabs){
 
+	chrome.windows.create('window.html', {
+    	'alwaysOnTop': true
+	});
+
     for (var i = 0; i < tabs.length; i++) {
     chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
     }
 
  	alert(JSON.stringify(tabs[0].title));
 });
-
