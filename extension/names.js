de6cddd9f2b4602b0008acc2de96696d56ba2df3
Deepak Padamata <deepakpadamata@gmail.com>
2014-10-25 22:46:50 +0530

    extensions panel

--- extension/names.js
+++ extension/names.js
@@ -1,11 +1,12 @@
 chrome.tabs.getAllInWindow(null, function(tabs){
 
-	chrome.windows.create('window.html', {
-    	'alwaysOnTop': true
-	});
+	chrome.windows.create({
+		'url': 'window.html',
+    	'type' : "panel"
+	})
 
     for (var i = 0; i < tabs.length; i++) {
-    chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
+    	chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
     }
 
  	alert(JSON.stringify(tabs[0].title));
