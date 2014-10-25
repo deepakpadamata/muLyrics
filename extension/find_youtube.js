b6dfba4548ff6549aef85eb3b73187c0216c99d4
abhiShandy <abhishek.sandy@gmail.com>
2014-10-26 04:31:59 +0530

    lyric to object conversion

--- extension/find_youtube.js
+++ extension/find_youtube.js
@@ -4,7 +4,7 @@ chrome.tabs.getAllInWindow(null, function(tabs){
     	chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
     	if((JSON.stringify(tabs[i].title).indexOf("YouTube")) > -1)
     		{
-    			c= i;		
+    			c = i;		
     			break;
 	    	}
 	}
@@ -14,17 +14,30 @@ chrome.tabs.getAllInWindow(null, function(tabs){
     else{
        alert(JSON.stringify(tabs[c].title));
     }  
-    chrome.windows.create({
-        'url': 'window.html',
-        'type' : "panel"
-    })
-
+    // chrome.windows.create({
+    //     'url': 'window.html',
+    //     'type' : "panel"
+    // });
     var req = new XMLHttpRequest();
-        req.open("GET", "/186041.lrc", true);
-        req.addEventListener("load", function(e) {
-            var txt = req.responseText
-            alert(txt);
-        }, false)
-        req.send(null);
+    req.open("GET", "/186041.lrc", true);
+    req.addEventListener("load", function(e) {
+        var txt = req.responseText;
+        console.log(txt);
+    }, false);
+    req.send(null);
+    // alert(txt);
+    // document.getElementById("showLRC").innerHTML = txt;
+    // chrome.pageCapture.saveAsMHTML(object details, function callback)
+    // var sl = document.getElementById("lrc");
+    // xmlDoc=loadXMLDoc("https://www.youtube.com/watch?v=P_WyB1Yunqw");
+    // x=xmlDoc.getElementsByTagName("title")[0];
+    // str = JSON.stringify(tabs[c].id);
+    // sl.innerHTML = str;
+    // console.log("--------"+x+"--------");
+ 	// console.log(JSON.stringify(tabs[c].innerHtml));
+    // chrome.windows.create({
+    //     'url': 'window.html',
+    //     'type' : "panel"
+    // })
 
 });
