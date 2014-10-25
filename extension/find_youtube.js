6efd46190553fb81868534eecdd8a24ffc79acb1
Deepak Padamata <deepakpadamata@gmail.com>
2014-10-26 02:15:41 +0530

    added ajax func

--- extension/find_youtube.js
+++ extension/find_youtube.js
@@ -1,5 +1,5 @@
 chrome.tabs.getAllInWindow(null, function(tabs){
-	var c;
+	var c = -1;
     for (var i = 0; i < tabs.length; i++) {	
     	chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
 
@@ -8,12 +8,24 @@ chrome.tabs.getAllInWindow(null, function(tabs){
     			c= i;		
     			break;
 	    	}
-	}    
- 	alert(JSON.stringify(tabs[c].title));
+	}
+    if (c == -1){
+    }
+    else{
+       alert(JSON.stringify(tabs[c].title));
+    }  
+    // chrome.windows.create({
+    //     'url': 'window.html',
+    //     'type' : "panel"
+    // })
 
-    chrome.windows.create({
-        'url': 'window.html',
-        'type' : "panel"
-    })
+    var req = new XMLHttpRequest();
+        req.open("GET", "/186041.lrc", true);
+        req.addEventListener("load", function(e) {
+            var txt = req.responseText
+            alert(txt);
+        }, false)
+        req.send(null);
 
+    
 });
