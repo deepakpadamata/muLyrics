7b219c05e9e36b74278439e87461469cd18cb85c
Deepak Padamata <deepakpadamata@gmail.com>
2014-10-25 21:42:31 +0530

    window borders removed

--- app/background.js
+++ app/background.js
@@ -2,8 +2,9 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
       'width': 300,
-      'height': 400
+      'height': 400,
     },
-    'alwaysOnTop': true
+    'alwaysOnTop': true,
+    frame: {type: "none"}
   });
 });
--- names.js
+++ names.js
@@ -1,9 +1,9 @@
 chrome.tabs.getAllInWindow(null, function(tabs){
 
     for (var i = 0; i < tabs.length; i++) {
-    chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });                         
+    chrome.tabs.sendRequest(tabs[i].id, { action: "xxx" });
     }
 
- 	alert(JSON.stringify(tabs));
+ 	alert(JSON.stringify(tabs[0].title));
 });
 
